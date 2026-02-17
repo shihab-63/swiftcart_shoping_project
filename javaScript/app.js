ea1601d8fbe940bf717ac88dc24bf0d843591065
shihab-63 <thecodzhub@gmail.com>
2026-02-17 08:02:09 +0600

    add choose section & navbar with responsive and add card from api

---
+++ javaScript/app.js
@@ -0,0 +1,73 @@
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const dropWordMenu = document.getElementById("dropdown-menu");
+const cardContainer = document.getElementById("card-container");
+
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+}
+
+if (dropWordMenu) {
+  dropWordMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+}
+
+// Load All Products
+const loadProducts = async () => {
+  const url = `https://fakestoreapi.com/products`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const threeData = data.slice(0, 3);
+    displayProduct(threeData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Display Products
+const displayProduct = (products) => {
+  cardContainer.innerHTML = "";
+  products.forEach((product) => {
+    cardContainer.innerHTML += `
+        <div
+            class="flex flex-col gap-2 border border-gray-300 rounded-xl hover:shadow-xl transition-shadow duration-400"
+        >
+            <div class="bg-gray-200 h-[300px] rounded-t-xl flex justify-center items-center">
+                <img
+                class="w-44 mx-auto hover:scale-110 transition-transform duration-500"
+                src="${product.image}"
+                alt=""
+                />
+            </div>
+                <div class="p-3">
+                  <div class="flex justify-between items-center">
+                    <p class="bg-indigo-200 rounded-lg px-1 text-indigo-600">${product.category}</p>
+                    <p class="text-gray-500">
+                      <span
+                        ><i class="fa-solid fa-star text-yellow-500"></i
+                      ></span>
+                      <span>${product.rating.rate} (${product.rating.count})</span>
+                    </p>
+                  </div>
+                  <h1 class="text-xl pt-4 truncate">${product.title}</h1>
+                  <p class="text-2xl font-medium py-4"><span>$</span><span>${product.price}</span></p>
+                  <div class="flex justify-between">
+                    <button class="btn px-10 md:px-14">
+                      <i class="fa-regular fa-eye"></i> Details
+                    </button>
+                    <button class="btn btn-primary px-10 md:px-14">
+                      <i class="fa-solid fa-cart-shopping"></i> Add
+                    </button>
+                  </div>
+                </div>
+              </div>
+    `;
+  });
+};
+
+loadProducts();
